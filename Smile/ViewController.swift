d990bc8eb011e2c06c1c8b80ec8ec06f8f396140
Dmitry Faddeev <faddeevd@gmail.com>
2016-06-08 09:02:16 +0300

    Initial Commit

---
+++ Smile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smile
+//
+//  Created by Dmitry Faddeev on 08/06/16.
+//  Copyright © 2016 Vladimir Nefedov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
